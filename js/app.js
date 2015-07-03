00107cd44c185eaf673a3f3114f4b69ae9e40bd0
Michels <michelsa@OLL43940Mac-Michels.home>
2015-07-02 20:56:15 -0500

    working diognal scrolling

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 $(function () {
-    $(window).scroll(function () {
-        $('#js-port').css({
-            'left': 
-        });
-    });
+  var lastScrollTop = 0,
+    $el = $('#js-port'),
+    leftVal = 2;
+    topVal = 5;
+  $(window).scroll(function () {
+    var st = $(this).scrollTop(),
+        elLeft = parseInt($('#js-port').css('left'), 10);
+        elTop = parseInt($('#js-port').css('top'), 10);
+    if (st > lastScrollTop){
+      // downscroll code
+      $('#js-port').css({'left': '+=50', 'top': '+=50'});
+    } else {
+      // upscroll code
+      $('#js-port').css({'left': '-=50', 'top': '-=50'});
+    }
+    lastScrollTop = st;
+  });
 });
